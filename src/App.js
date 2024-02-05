f5adb929749d0299a220a36a112b062c24b0c223
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2024-02-06 00:21:52 +0530

    Updated routs for example usage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 // src/App.js
 import React from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
-import Contact from "./components/Contact";
 
 const router = createBrowserRouter([
   {
