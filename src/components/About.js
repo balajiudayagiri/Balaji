1cc347cc97298e53fa526f846bac5e4cc27879c3
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2024-02-06 00:18:22 +0530

    Updated routs for example usage

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
     <div>
       <h2>About</h2>
       <p>This is the about page.</p>
+      <Link to="/">home</Link>
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
       <p>Welcome to the home page!</p>
+      <Link to="about">about</Link>
     </div>
   );
 };
