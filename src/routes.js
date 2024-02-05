d31e70702f9671d62c4b49728ac4a82f543d3f89
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2024-02-06 02:49:30 +0530

    tailwind css setup

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,29 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Route, Routes, createBrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Contact from "./components/Contact";
+
+import App from "./App";
+import ErrorPage from "./ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "about",
-    element: <About />,
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "home/",
+        element: <Home />,
+      },
+      {
+        path: "about/",
+        element: <About />,
+      },
+      {
+        path: "contact/",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
