4bbd55da4628652f948a0f4e8ea18b618f8b8e17
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2024-02-05 22:11:02 +0530

    Updated workflow

--- src/App.js
+++ src/App.js
@@ -1,37 +1,8 @@
 // src/components/Router.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-      dsfddsdsadas BALAJI U
-      <Router>
-        <Routes>
-          <Route path="/" exact component={Home} />
-          <Route path="/page2" component={Page2} />
-        </Routes>
-      </Router>
-    </div>
-  );
+  return <div>dsfddsdsadas BALAJI U succses</div>;
 }
 
 export default App;
-
-const Home = () => {
-  return (
-    <div>
-      <h1>Home Page</h1>
-      {/* Add your home page content here */}
-    </div>
-  );
-};
-
-const Page2 = () => {
-  return (
-    <div>
-      <h1>Page 2</h1>
-      {/* Add your second page content here */}
-    </div>
-  );
-};
