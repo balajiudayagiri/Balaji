fb38d536259f52f9287c4fac4597ffebfdf46c37
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2024-02-06 02:52:26 +0530

    tailwind css setup

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { Route, Routes, createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
