ce31991facfeacfbf2031823fd4ede95af6086e3
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2024-02-05 16:35:32 +0530

    Updated build

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div>
-      dsfddsdsadas BALAJI
+      dsfddsdsadas BALAJI U
       <Router>
         <Routes>
           <Route path="/" exact component={Home} />
