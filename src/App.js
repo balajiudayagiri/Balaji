199bb399e4126f5d8b31306ccbe7ae2e233ec22f
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2024-02-05 23:55:21 +0530

    Updated routs for example usage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-// src/components/Router.js
+// src/App.js
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
-function App() {
-  return <div>dsfddsdsadas BALAJI U succses</div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <hr />
+
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
+  );
+};
 
 export default App;
